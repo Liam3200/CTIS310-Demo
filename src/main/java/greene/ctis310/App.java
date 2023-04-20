f8123c2e2e364d0bd0f9054f348da8851defea36
Liam Greene <wgreene@guilford.edu>
2023-04-20 11:16:31 -0400

    copilot demo

---
+++ src/main/java/greene/ctis310/App.java
@@ -0,0 +1,46 @@
+package greene.ctis310;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        // intialize a dog with random attributes
+        Dog dog = new Dog("Jeff", 4, "Bulldog", "Black");
+        // create an arraylist of dogs and add the dog to it
+        ArrayList<Dog> dogs = new ArrayList<Dog>();
+        dogs.add(dog);
+        // create a loop that intializes and adds 5 dogs to the arraylist
+        for (int i = 0; i < 5; i++) {
+            dogs.add(new Dog());
+        }
+
+        // give each dog a random name without a loop
+        dogs.get(1).setName("Bob");
+        dogs.get(2).setName("Joe");
+        dogs.get(3).setName("Steve");
+        dogs.get(4).setName("Bill");
+        dogs.get(5).setName("John");
+
+        // add a for each loop that prints out the attributes of each dog
+        for (Dog d : dogs) {
+            System.out.println(d);
+        }
+
+        // create a loop that makes each dog bark a random number of times
+        for (Dog d : dogs) {
+            for (int i = 0; i < (int) (Math.random() * 10); i++) {
+                d.bark();
+            }
+        }
+
+        // create a loop that prints out the number of times each dog barked
+        for (Dog d : dogs) {
+            System.out.println(d.getName() + " barked " + d.getBarked() + " times.");
+        }
+
+    }
+}
